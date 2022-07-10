8dfa17aa491a616d9d4c738a5025928f915ed361
HenriqueEduardo1 <henriqueeduardo1123@gmail.com>
2022-07-10 19:24:14 -0300

    Foi feito o carrossel com relatos dos clientes

---
+++ script/loop.js
@@ -0,0 +1,16 @@
+let tempo = 5000
+let clienteAtual = 0
+let clientes = document.querySelectorAll(".loop-clientes div")
+let maxClientes = clientes.length - 1
+
+function nextCliente() {
+    clientes[clienteAtual].classList.remove("cliente-ativo")
+    clienteAtual >= maxClientes ? clienteAtual = 0 : clienteAtual++
+    clientes[clienteAtual].classList.add("cliente-ativo")
+}
+
+function startLoop() {
+    setInterval(() => { nextCliente() }, tempo)
+}
+
+window.addEventListener("load", startLoop)
