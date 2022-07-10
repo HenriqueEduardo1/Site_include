d492f7c436c3388fcc5a6b6615a081bb4161e5e7
HenriqueEduardo1 <henriqueeduardo1123@gmail.com>
2022-07-09 21:05:23 -0300

    ajuste menu

--- script/menu.js
+++ script/menu.js
@@ -3,8 +3,8 @@ let menu_on = true;
 const cabecalho = document.querySelector(".cabecalho")
 const menuHamb = document.querySelector(".menu-hamb")
 
-menuHamb.addEventListener("click", () => {
+function openMenu() {
     document.body.style.overflow = menu_on ? "hidden" : "initial"
     cabecalho.classList.toggle("cliked", menu_on)
     menu_on = !menu_on;
-})
+}
